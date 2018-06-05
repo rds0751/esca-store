95e4be82ae1ab1760669334e2132c86ba47988b5
Ripudaman Singh <rsingh_bemba16@thapar.edu>
2018-06-05 13:00:05 +0530

    hatt bhosad

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn escashop.wsgi --log-file -
---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\gaurav~1\desktop\aboutu~1\frontend\esca-s~1\esca-s~1\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
